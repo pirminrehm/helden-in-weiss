e1d82a9db12f5210f71b06c40771a71abb6adacc
PKief <philipp.kief@gmx.de>
2020-03-24 19:11:55 +0100

    Add title service to adjust title for each routed component

--- apps/helper-app/src/app/app.component.ts
+++ apps/helper-app/src/app/app.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'wir-vs-virus-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  title = 'Helden in Weiss';
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) { }
+
+  ngOnInit() {
+    const appTitle = this.titleService.getTitle();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        const child = this.activatedRoute.firstChild;
+        if (child.snapshot.data['title']) {
+          return this.title + ' | ' + child.snapshot.data['title'];
+        }
+        return appTitle;
+      })
+    ).subscribe((title: string) => {
+      this.titleService.setTitle(title);
+    });
+  }
+}
