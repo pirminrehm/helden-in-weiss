bc58b60fca2fee87681414148efb6f827a48d6d8
Pirmin Rehm <pirmin.rehm@capgemini.com>
2020-03-25 22:03:51 +0100

    cleanup institution service mock data

--- apps/helper-app/src/services/institution.service.ts
+++ apps/helper-app/src/services/institution.service.ts
@@ -17,52 +17,12 @@ export class InstitutionService {
     zipCode: string,
     radius: number
   ): Observable<Institution[]> {
-    if (this.useMockData) {
-      return this.getMockData();
-    }
     return this.http.get<Institution[]>(
       `/api/institution?searchTerm=${searchTerm}&zipcode=${zipCode}&radius=${radius}`
     );
   }
 
-  // public getByZipCode(zipCode): Observable<Institution[]> {
-  //   return this.http.get<Institution[]>(`/api/institution?zipcode=${zipCode}`);
-  // }
-
-  // public getByZipCodeAndRadius(
-  //   zipCode: number,
-  //   radius: number
-  // ): Observable<Institution[]> {
-  //   return this.http.get<Institution[]>(
-  //     `/api/institution?zipcode=${zipCode}&radius=${radius}`
-  //   );
-  // }
-
   public create(institution: Institution) {
     return this.http.post<Institution[]>('/api/institution', institution);
-  }
-
-  private getMockData() {
-    return of([
-      {
-        name: 'Institution',
-        firstname: 'Institution',
-        email: 'Institution',
-        zipcode: 70569,
-        city: 'Institution',
-        age: 27,
-        phone: '-',
-        contact: {
-          name: 'string',
-          firstname: 'string',
-          phone: 'string',
-          email: 'string'
-        },
-        description: 'Wir benötigen Helfer.',
-        title: 'EInstitutionInstitutionInstitution',
-        created: '2020-03-21T15:36:50.756Z',
-        qualification: 'Institution'
-      }
-    ]).pipe(delay(1000));
   }
 }
