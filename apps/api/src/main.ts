5c8a70797a374b6a1f31b41105de78fe9c79a156
PKief <philipp.kief@gmx.de>
2020-03-26 22:08:41 +0100

    Add helmet middleware

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,8 +4,9 @@
  */
 
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
 import * as rateLimit from 'express-rate-limit';
+import { AppModule } from './app/app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,9 @@ async function bootstrap() {
       max: 500 // limit each IP to 500 requests per windowMs
     })
   );
+
+  // protect app from some well-known web vulnerabilities by setting HTTP headers
+  app.use(helmet());
 
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
