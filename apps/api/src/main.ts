e4f3c453927e6ac458583fe98dfa06ec94460a67
PKief <philipp.kief@gmx.de>
2020-03-26 22:05:51 +0100

    Add rate limiter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,12 +4,20 @@
  */
 
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 500 // limit each IP to 500 requests per windowMs
+    })
+  );
+
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
   await app.listen(port, () => {
